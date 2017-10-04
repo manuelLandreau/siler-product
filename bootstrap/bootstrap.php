02402bc017a43308f4eb0ef1f320b9b080c89efd
manuel.landreau <thma1999>
2017-10-04 13:27:04 +0200

    fix static files issues

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,4 +1,15 @@
 <?php
+
+// Static files
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Siler\Dotenv\init(__DIR__ . '/../');
